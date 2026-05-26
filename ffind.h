9cbfbc95dc7bc7095954946ba16ea1184077f261
xz3k <maxim_j037@web.de>
2026-05-26 20:12:42 +0000

    Create ffind.h

---
+++ ffind.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+
+#define DB_PATH "/usr/share/ffind/ffind.db"
+#define PATH_MAX 4096
+
+int main(int argc, char* argv[]);
